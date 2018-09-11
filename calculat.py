2610f1ddecf7a8c4c8b35ccf864cf865a3f9dd5c
ali4109 <35405852+ali4109@users.noreply.github.com>
2018-09-11 07:40:10 -0700

    Add files via upload

---
+++ calculat.py
@@ -0,0 +1,129 @@
+def menu():
+    r = str(input("say me you want use calculate or factorial:say white (1=calculate) or (2=factorial)"))
+    if r == '1': 
+        def mosbat():
+            x = int(input("say me first number:"))
+            y = int(input("say me second number:"))
+            o = str(input("say white((+/*-))"))
+            if o == "+":
+                a = (x + y)
+                print (a)
+            if o == "-":
+                aa = (x - y)
+                print (aa)
+            if o == "*":
+                aaa = (x * y)
+                print (aaa)
+            if o == "/":
+                aaaa = (x / y)
+                print (aaaa)
+            if o == "**":
+                aaaaa = (x ** y)
+                print (aaaaa)
+        mosbat()
+    if r == "2":
+        def factoril():
+            x = int(input("addi ke mikhay factoril ono caluculate beshe ro bde:"))
+            num = 1
+            while x >= 1:
+                num = num * x
+                x = x - 1
+                print(num)
+        factoril()
+        def mosbat():
+            x = int(input("say me first number:"))
+            y = int(input("say me second number:"))
+            o = str(input("say white((+/*-))"))
+            if o == "+":
+                a = (x + y)
+                print (a)
+            if o == "-":
+                aa = (x - y)
+                print (aa)
+            if o == "*":
+                aaa = (x * y)
+                print (aaa)
+            if o == "/":
+                aaaa = (x / y)
+                print (aaaa)
+            if o == "**":
+                aaaaa = (x ** y)
+                print (aaaaa)
+        mosbat()
+    if r == "2":
+        def factoril():
+            x = int(input("addi ke mikhay factoril ono caluculate beshe ro bde:"))
+            num = 1
+            while x >= 1:
+                num = num * x
+                x = x - 1
+                print(num)
+        factoril()
+        def mosbat():
+            x = int(input("say me first number:"))
+            y = int(input("say me second number:"))
+            o = str(input("say white((+/*-))"))
+            if o == "+":
+                a = (x + y)
+                print (a)
+            if o == "-":
+                aa = (x - y)
+                print (aa)
+            if o == "*":
+                aaa = (x * y)
+                print (aaa)
+            if o == "/":
+                aaaa = (x / y)
+                print (aaaa)
+            if o == "**":
+                aaaaa = (x ** y)
+                print (aaaaa)
+        mosbat()
+    if r == "2":
+        def factoril():
+            x = int(input("addi ke mikhay factoril ono caluculate beshe ro bde:"))
+            num = 1
+            while x >= 1:
+                num = num * x
+                x = x - 1
+                print(num)
+        factoril()
+        def mosbat():
+            x = int(input("say me first number:"))
+            y = int(input("say me second number:"))
+            o = str(input("say white((+/*-))"))
+            if o == "+":
+                a = (x + y)
+                print (a)
+            if o == "-":
+                aa = (x - y)
+                print (aa)
+            if o == "*":
+                aaa = (x * y)
+                print (aaa)
+            if o == "/":
+                aaaa = (x / y)
+                print (aaaa)
+            if o == "**":
+                aaaaa = (x ** y)
+                print (aaaaa)
+        mosbat()
+    if r == "2":
+        def factoril():
+            x = int(input("addi ke mikhay factoril ono caluculate beshe ro bde:"))
+            num = 1
+            while x >= 1:
+                num = num * x
+                x = x - 1
+                print(num)
+        factoril()
+menu()
+        
+
+
+
+
+
+
+
+
